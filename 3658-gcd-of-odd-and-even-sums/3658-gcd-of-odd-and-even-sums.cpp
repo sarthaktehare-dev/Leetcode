b730b02c42277895f4ce649a86f2f63318608144
Sarthak Tehare <teharesarthak@gmail.com>
2026-07-15 23:19:26 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -2,31 +2,11 @@ class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
         
-        int s1 = 0, s2 = 0;
-        if(n == 1) return 1;
-        int num1 = n;
-        int num2 = n;
-        int i = 1;
-        while(num1 > 0){
-            s1 += i;
-            i += 2;
-            num1--;
-        }
-        int j = 2;
-        while(num2 > 0){
-            s2 += j;
-            j += 2;
-            num2--;
-        }
 
-        if(s1 > s2){
-            s1 = s1 % s2;
-            return s1;
-        }
-        else{
-            s2 = s2 % s1;
-            return s2;
-        }
+        if(n == 1) return n;
+
+        if(n %2 == 0) return  n;
+        else return n ;
 
     }
-};
+}; 
