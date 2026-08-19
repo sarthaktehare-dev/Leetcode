e93777ffada32350c11791411789d1f2fe85bd06
Sarthak Tehare <teharesarthak@gmail.com>
2026-08-20 01:06:37 +0530

    Time: 0 ms (100%), Space: 16.8 MB (33.15%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,27 +12,28 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         
+    if(head == NULL || head -> next == NULL) return head;
         vector<int> temp;
-        ListNode* t = head;
-  if(head  == NULL || head -> next == NULL) return head;
-    
-        while(t != NULL){
-            temp.push_back(t -> val);
-            t = t -> next;
-        }
-          k = k % temp.size();
+        ListNode* temp1 = head;
 
-          reverse(temp.begin() , temp.end());
-          reverse(temp.begin() , temp.begin() + k);
-          reverse(temp.begin() + k , temp.end());
-
-          ListNode* ans = new ListNode(-1);
-          ListNode* temp1 = head;
-
-          for(int i = 0; i < temp.size(); i++){
-            temp1 -> val = temp[i];
+        while(temp1){
+            temp.push_back(temp1 -> val);
             temp1 = temp1 -> next;
-          }
-          return head;
+        }
+           k = k % temp.size();
+           
+        reverse(temp.begin() , temp.end());
+        reverse(temp.begin() , temp.begin() + k);
+        reverse(temp.begin() + k , temp.end());
+
+        ListNode*  ans = new ListNode(-1);
+        ListNode* temp3 = ans;
+
+        for(int i = 0; i < temp.size(); i++){
+            ListNode* newNode = new ListNode(temp[i]);
+            temp3 -> next = newNode;
+            temp3 = temp3 -> next;
+        } 
+        return ans -> next;
     }
 };
