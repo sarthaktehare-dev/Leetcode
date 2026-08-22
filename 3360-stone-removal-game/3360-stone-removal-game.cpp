57d79bab06d6f08fa0f4a4ddea3157cd21146b06
Sarthak Tehare <teharesarthak@gmail.com>
2026-08-22 16:38:20 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.19%) - LeetHub

--- 3360-stone-removal-game/3360-stone-removal-game.cpp
+++ 3360-stone-removal-game/3360-stone-removal-game.cpp
@@ -2,31 +2,22 @@ class Solution {
 public:
     bool canAliceWin(int n) {
         
-        vector<int> v;
- int cnt = 0;
- int remove = 10;
- if(n < 10) return false;
+        
+        int cnt = 0;
+        int remove = 10;
+        if(n < 10) return false;
  
        while(n >= remove){
          n = n - remove;
          cnt++;
          remove--;
-          //v.push_back(1);
+          
        }   
          if(cnt %2 == 0) return false;
         else return true;
 
        
 
-    //    int alice = 0;
-    //    int bob = 0;
-
-    //   for(int i = 0; i < v.size(); i++){
-    //     if(i %2 == 0 && v[i] == 1) alice++;
-    //     else bob++;
-    //   }
-
-    //   if(alice > bob) return true;
-    //   else return false;
+    
     }
 };
